ca1951283642ef2bc524e36832195417fe35f937
Kevin Miller Jr <kevinmillerjr@gmail.com>
2023-01-21 14:38:42 -0800

    Updated query

--- .history/src/lib/ghost_20230120215645.ts
+++ .history/src/lib/ghost_20230120215645.ts
@@ -1,5 +1,6 @@
 import GhostContentAPI from "@tryghost/content-api";
 
+// Create API instance with site credentials
 export const ghostClient = new GhostContentAPI({
   url: "https://my.kerimnur.com",
   key: import.meta.env.CONTENT_API_KEY,
