b75e24579b319cb5125a959f687e8694ef0411db
Kevin Miller Jr <kevinmillerjr@gmail.com>
2023-01-22 15:26:12 -0800

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,24 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://kerimnur.com",
+  integrations: [
+    tailwind(),
+    sitemap({
+      customPages: [
+        "https://kerimnur.com",
+        "https://kerimnur.com/about-kerimnur",
+        "https://kerimnur.com/post",
+        "https://kerimnur.com/mosques",
+        "https://kerimnur.com/islam-resources",
+        "https://kerimnur.com/contact",
+      ],
+    }),
+  ],
   output: "server",
   adapter: vercel(),
 });
