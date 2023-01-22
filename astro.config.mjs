73609b0d8aeced6308a76fc6ca34a8a5a74a1f18
Kevin Miller Jr <kevinmillerjr@gmail.com>
2023-01-21 21:41:26 -0800

    Removed sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  // ...
-  site: "https://kerimnur.com",
-  integrations: [sitemap()],
   integrations: [tailwind()],
   output: "server",
   adapter: vercel(),
