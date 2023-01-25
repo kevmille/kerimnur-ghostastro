55e3c22fa67f50f1b33120639879b41bbc5d11ea
Kevin Matsunaga <kevinmillerjr@gmail.com>
2023-01-24 17:20:55 -0800

    Updated to Astro 2.0.0

--- .history/src/lib/ghost_20230120215648.ts
+++
@@ -1,7 +0,0 @@
-import GhostContentAPI from "@tryghost/content-api";
-
-export const ghostClient = new GhostContentAPI({
-  url: "https://my.kerimnur.com",
-  key: import.meta.env.CONTENT_API_KEY,
-  version: "v5.0",
-});
